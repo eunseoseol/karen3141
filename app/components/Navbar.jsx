6dc01f4ddf78cdb430602a8cc357cdf6e4071931
Eunseo Seol <147463468+eunseoseol@users.noreply.github.com>
2024-06-11 11:15:21 +0900

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -60,8 +60,7 @@ const Navbar = () => {
             setProfileImage(userData.profileImage);
           }
         }
-
-        // Fetch notifications
+ 
         await fetchNotifications();
       }
       setLoading(false);
@@ -90,7 +89,7 @@ const Navbar = () => {
           <li className="p-2 cursor-pointer">
             <Link href="/">
               <span className="text-xl lg:text-1.5xl font-semibold text-black-600">
-                암호화폐 커뮤니티
+                암호화폐 백과사전
               </span>
             </Link>
           </li>
